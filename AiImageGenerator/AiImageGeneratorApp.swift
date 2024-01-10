2d26c798d78d3438e268c122e16877c58b7dec27
omursnck <omrsnr12@icloud.com>
2024-01-10 20:19:33 +0300

    Initial Commit

---
+++ AiImageGenerator/AiImageGeneratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  AiImageGeneratorApp.swift
+//  AiImageGenerator
+//
+//  Created by Ömür Şenocak on 10.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct AiImageGeneratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
